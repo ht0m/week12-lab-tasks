bc8c4296066e424c7af37fe5d3c4915ada373967
ht0m <hartht@gmail.com>
2022-01-14 16:37:55 +0100

    day05 - Stringstatistics

--- src/test/java/day05/StringStatisticsTest.java
+++ src/test/java/day05/StringStatisticsTest.java
@@ -2,6 +2,8 @@ package day05;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringStatisticsTest {
@@ -11,5 +13,6 @@ class StringStatisticsTest {
         StringStatistics stringStatistics = new StringStatistics();
         System.out.println(stringStatistics.getStatistic("almafa"));
         assertEquals(3,stringStatistics.getStatistic("almafa").get('a'));
+
     }
 }
