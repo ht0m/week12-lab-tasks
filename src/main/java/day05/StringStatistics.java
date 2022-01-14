988da14a9317fb1a92bb216c10caf512b6268e84
ht0m <hartht@gmail.com>
2022-01-14 15:43:58 +0100

    day05 - Stringstatistics

---
+++ src/main/java/day05/StringStatistics.java
@@ -0,0 +1,18 @@
+package day05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringStatistics {
+
+    public Map<Character,Integer> getStatistic(String text) {
+        Map<Character,Integer> stringStatistic = new HashMap<>();
+        for (Character c: text.toCharArray()) {
+            if (!stringStatistic.containsKey(c)) {
+                stringStatistic.put(c,0);
+            }
+            stringStatistic.put(c,stringStatistic.get(c)+1);
+        }
+        return stringStatistic;
+    }
+}
---
+++ src/test/java/day05/StringStatisticsTest.java
@@ -0,0 +1,15 @@
+package day05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringStatisticsTest {
+
+    @Test
+    void testStringStatistics() {
+        StringStatistics stringStatistics = new StringStatistics();
+        System.out.println(stringStatistics.getStatistic("almafa"));
+        assertEquals(3,stringStatistics.getStatistic("almafa").get('a'));
+    }
+}
