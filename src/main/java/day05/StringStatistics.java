d29c4efb37f856f72875a8edfe9c844ac508b41f
ht0m <hartht@gmail.com>
2022-01-14 15:46:08 +0100

    day05 - Stringstatistics

--- src/main/java/day05/StringStatistics.java
+++ src/main/java/day05/StringStatistics.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 public class StringStatistics {
 
-    public Map<Character,Integer> getStatistic(String text) {
-        Map<Character,Integer> stringStatistic = new HashMap<>();
-        for (Character c: text.toCharArray()) {
+    public Map<Character, Integer> getStatistic(String text) {
+        Map<Character, Integer> stringStatistic = new HashMap<>();
+        for (Character c : text.toCharArray()) {
             if (!stringStatistic.containsKey(c)) {
-                stringStatistic.put(c,0);
+                stringStatistic.put(c, 0);
             }
-            stringStatistic.put(c,stringStatistic.get(c)+1);
+            stringStatistic.put(c, stringStatistic.get(c) + 1);
         }
         return stringStatistic;
     }
